b1c41750ba11b0574f59a534bb3dbad6b1a94dc8
Zhenghao Li <lizhenghao99@outlook.com>
2021-10-31 17:29:16 -0700

    UI update

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,6 +1,7 @@
-import { Button, Flex, Heading, Text, useBreakpointValue, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Button, Center, Flex, Heading, Text, useBreakpointValue, useColorModeValue, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import * as React from 'react';
+import { Card } from '../components/layouts/Card';
 import { Logo } from '../components/media/Logo';
 
 const Welcome = (props) => {
@@ -12,7 +13,6 @@ const Welcome = (props) => {
                 minH="100vh"
                 direction={'column'}
                 alignItems={'center'}
-                px={{ base: '2rem', lg: '20rem' }}
                 py={'3rem'}
             >
                 <Logo
@@ -24,24 +24,28 @@ const Welcome = (props) => {
                     }}
                 />
                 <Heading mb={'3rem'} fontWeight={'extrabold'}>Welcome to Concord</Heading>
-                <VStack spacing={{ base: '1rem', lg: '3rem' }} alignItems={'flex-start'}
-                        mb={{ base: '3rem', lg: '5rem' }}>
-                    <Text fontSize={textSize}>
-                        Hi there, thanks for visiting <strong>Concord</strong>, a web application that is created to
-                        help couples and groups of friends to discover and decide on stuffs to enjoy together.
-                    </Text>
-                    <Text fontSize={textSize}>
-                        Currently, this app supports Netflix movies and series to select from.
-                    </Text>
-                </VStack>
-                <NextLink href={'/auth/login'}>
-                    <Button
-                        colorScheme={'blue'}
-                        size={'lg'}
-                    >
-                        Launch Concord
-                    </Button>
-                </NextLink>
+                <Card maxW={{ base: '90%', lg: '70%', xl: '50%' }}>
+                    <VStack spacing={{ base: '1rem', lg: '3rem' }} alignItems={'flex-start'}
+                            mb={{ base: '3rem', lg: '5rem' }}>
+                        <Text fontSize={textSize}>
+                            Hi there, thanks for visiting <strong>Concord</strong>, a web application that is created to
+                            help couples and groups of friends to discover and decide on stuffs to enjoy together.
+                        </Text>
+                        <Text fontSize={textSize}>
+                            Currently, this app supports Netflix movies and series to select from.
+                        </Text>
+                    </VStack>
+                    <Center>
+                        <NextLink href={'/auth/login'}>
+                            <Button
+                                colorScheme={'blue'}
+                                size={'lg'}
+                            >
+                                Launch Concord
+                            </Button>
+                        </NextLink>
+                    </Center>
+                </Card>
             </Flex>
         </>
     );
